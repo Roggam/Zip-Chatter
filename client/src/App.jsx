379f7711e864d8d951c4b90a0f3b1171f15951de
Rogelio Gamboa Jr <roggam@yahoo.com.mx>
2021-12-18 15:28:34 -0500

    added functionality to Channel container and Channel Inner

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
+import 'stream-chat-react/dist/css/index.css';
 import './App.css';
 
 import { ChannelListContainer, ChannelContainer, Auth } from './components'; //takes care of importing all components
@@ -28,16 +29,27 @@ if (authToken) {
 }
 
 export const App = () => {
+const [createType, setCreateType] = useState("");
+const [isCreating, setIsCreating] = useState(false);
+const [isEditing, setIsEditing] = useState(false);
+
     if (!authToken) return <Auth />
 
     return (
         <div className='app__wrapper'>
             <Chat client={client} theme="theme light">
                 <ChannelListContainer //component
-
+                    isCreating={isCreating}
+                    setIsCreating={setIsCreating}
+                    setCreateType={setCreateType}
+                    setIsEditing={setIsEditing}
                 />
                 <ChannelContainer //component
-
+                    isCreating={isCreating}
+                    setIsCreating={setIsCreating}
+                    isEditing={isEditing}
+                    setIsEditing={setIsEditing}
+                    createType={createType}
                 />
 
             </Chat>
