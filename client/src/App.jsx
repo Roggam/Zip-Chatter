18ead2c2f51a3dead6a8adf931d31468aae68b10
Rogelio Gamboa Jr <roggam@yahoo.com.mx>
2021-12-18 14:14:34 -0500

    added logout button functionality to logout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import{ StreamChat } from 'stream-chat';
+import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
@@ -14,33 +14,33 @@ const authToken = cookies.get("token");
 
 const client = StreamChat.getInstance(apiKey); //passing api key
 
-if(authToken){
+if (authToken) {
     client.connectUser({
         id: cookies.get('userId'),
-        name : cookies.get('username'),
+        name: cookies.get('username'),
         fullName: cookies.get('fullName'),
         image: cookies.get('avatarURL'),
         hashedPassword: cookies.get('hashedPassword'),
         phoneNumber: cookies.get('phoneNumber'),
-        
+
 
     }, authToken);
 }
 
 export const App = () => {
-if(!authToken) return <Auth />
+    if (!authToken) return <Auth />
 
     return (
         <div className='app__wrapper'>
-           <Chat client={client} theme="theme light">
-               <ChannelListContainer //component
-               
-               />
-            <ChannelContainer //component
-               
-               />
+            <Chat client={client} theme="theme light">
+                <ChannelListContainer //component
 
-           </Chat>
+                />
+                <ChannelContainer //component
+
+                />
+
+            </Chat>
         </div>
     );
 }
--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -6,72 +6,86 @@ import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
 import ZipChatterIcon from '../assets/zipChatterIcon.png';
 import LogoutIcon from '../assets/logout.png';
 
-const SideBar = () => (
+const cookies = new Cookies();
+
+const SideBar = ({ logout }) => (
     <div className='channel-list__sidebar'>
         <div className='channel-list__sidebar__icon1'>
-           <div className='icon1__inner'>
-            <img src={ZipChatterIcon} alt='ZipChatter' width="30"/>
-           </div>
+            <div className='icon1__inner'>
+                <img src={ZipChatterIcon} alt='ZipChatter' width="30" />
+            </div>
         </div>
         <div className='channel-list__sidebar__icon2'>
-           <div className='icon1__inner'>
-            <img src={LogoutIcon} alt='Logout' width="30"/>
-           </div>
+            <div className='icon1__inner' onClick={logout}>
+                <img src={LogoutIcon} alt='Logout' width="30" />
+            </div>
         </div>
 
     </div>
 )
 const CompanyHeader = () => (
-<div className='channel-list__header'>
-<p className='channel-list__header__text'>Zip Chatter</p>
-</div>
+    <div className='channel-list__header'>
+        <p className='channel-list__header__text'>Zip Chatter</p>
+    </div>
 
 )
 
 
- const ChannelListContainer = () => {
+const ChannelListContainer = () => {
+    const logout = () => {
+        cookies.remove("token");
+        cookies.remove('userId');
+        cookies.remove('username');
+        cookies.remove('fullName');
+        cookies.remove('avatarURL');
+        cookies.remove('hashedPassword');
+        cookies.remove('phoneNumber');
+        window.location.reload();
+
+    }
+
     return (
         //react fragment. it will render anything inside of it
         <>
-           <SideBar />
-           <div className='channel-list__list__wrapper'>
-               <CompanyHeader/>
-               <ChannelSearch/>
-               <ChannelList
-               filters={{}}
-               channelRenderFilterFn={() => {}}
-               List={ (listProps) => ( //custom TeamChannelList component will get all props from ChannelList
-                    <TeamChannelList
-                    {... listProps}
-                    type="team"
-                    />
-               )}
-                 Preview={(previewProps) => (
-                <TeamChannelPreview 
-                {...previewProps}
-                type="team"
+            <SideBar logout={logout} />
+            <div className='channel-list__list__wrapper'>
+                <CompanyHeader />
+                <ChannelSearch />
+                <ChannelList
+                    filters={{}}
+                    channelRenderFilterFn={() => { }}
+                    List={(listProps) => ( //custom TeamChannelList component will get all props from ChannelList
+                        <TeamChannelList
+                            {...listProps}
+                            type="team"
+                        />
+                    )}
+                    Preview={(previewProps) => (
+                        <TeamChannelPreview
+                            {...previewProps}
+                            type="team"
+                        />
+                    )}
                 />
-               )}   
-               />
                 <ChannelList // this component will display direct messages
-               filters={{}}
-               channelRenderFilterFn={() => {}}
-               List={ (listProps) => ( 
-                    <TeamChannelList
-                    {... listProps}
-                    type="messaging"
-                    />
-               )}
-               
-               Preview={(previewProps) => (
-                <TeamChannelPreview 
-                {...previewProps}
-                type="messaging"
+                    filters={{}}
+                    channelRenderFilterFn={() => { }}
+                    List={(listProps) => (
+                        <TeamChannelList
+                            {...listProps}
+                            type="messaging"
+                        />
+                    )}
+
+                    Preview={(previewProps) => (
+                        <TeamChannelPreview
+                            {...previewProps}
+                            type="messaging"
+                        />
+                    )}
                 />
-               )}   
-               />
-               
-           </div>
+
+            </div>
         </>
     );
 }
